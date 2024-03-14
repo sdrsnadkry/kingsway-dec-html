c4b21238d2648c097c522dd892bd6d85839366e0
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-03-14 09:22:09 +0545

    learned about variable

---
+++ index.js
@@ -0,0 +1,31 @@
+//Variables and constants
+
+//const
+
+const name = "John";
+console.log("name", name);
+
+
+// const name = "Pawan"
+
+
+//let
+
+let age = 25;
+console.log(age);
+age = 26;
+console.log(age);
+
+// let age = 27
+
+//var
+
+var address = "Nepal";
+console.log(address);
+address = "USA";
+console.log(address);
+
+
+var address = "India"
+
+console.log(address)
