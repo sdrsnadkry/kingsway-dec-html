b1dd704a0111d8174aeccc1641098f12e57a039a
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-03-27 09:16:40 +0545

    assignments

--- assignment.js
+++ assignment.js
@@ -4,19 +4,34 @@ function convertToUppercase(lowerCaseString) {
 
 // console.log(convertToUppercase("apple"));
 
-
 function reverseAString(string) {
+    const slitArray = string.split("");
 
-    const slitArray = string.split("")
+    const reverseArray = slitArray.reverse();
 
-    const reverseArray = slitArray.reverse()
+    const reverseString = reverseArray.join("");
 
-    const reverseString = reverseArray.join("")
-
-    return reverseString
+    return reverseString;
 
     // return string.split("").reverse().join("")
-
 }
 
-console.log(reverseAString("Heena"))
+// const number = [5, 3, 2, 4, 6, 0, 7, 8, 9];
+
+// let sum = 0;
+
+// for (let i = 0; i < number.length; i++) {
+//   sum = sum + number[i];
+// }
+
+// console.log(sum);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     // console.log(numbers[i])
+
+//     if (numbers[i] % 2 === 0) {
+//         console.log(numbers[i] + " is even");
+//     }
+// }
